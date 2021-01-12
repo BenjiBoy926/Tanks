0268aa15c4253a1e75415aca62cfc3842c12be81
Codey Huntting <creativecodey@gmail.com>
2021-01-12 14:10:47 -0800

    Tanks complete for 2p keyboard play

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] tankColliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        // Damage each of the tanks in the tanks hit
+        foreach(Collider tank in tankColliders)
+        {
+            // Add an explosion force to the tank's rigidbody
+            Rigidbody tankRigidbody = tank.GetComponent<Rigidbody>();
+            if (!tankRigidbody) continue;
+            tankRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            // Reduce health of the tank health script
+            TankHealth tankHealth = tank.GetComponent<TankHealth>();
+            if (!tankHealth) continue;
+            tankHealth.TakeDamage(CalculateDamage(tankRigidbody.position));
+        }
+
+        // Unparent the particles from the shell.
+        m_ExplosionParticles.transform.parent = null;
+
+        // Play the particle system.
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Create a vector from the shell to the target.
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        // Calculate the distance from the shell to the target.
+        float explosionDistance = explosionToTarget.magnitude;
+
+        // Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        // Calculate damage as this proportion of the maximum possible damage.
+        float damage = relativeDistance * m_MaxDamage;
+
+        // Make sure that the minimum damage is always 0.
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;         
+    public float m_Speed = 12f;            
+    public float m_TurnSpeed = 180f;       
+    public AudioSource m_MovementAudio;    
+    public AudioClip m_EngineIdling;       
+    public AudioClip m_EngineDriving;      
+    public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;     
+    private string m_TurnAxisName;         
+    private Rigidbody m_Rigidbody;         
+    private float m_MovementInputValue;    
+    private float m_TurnInputValue;        
+    private float m_OriginalPitch;         
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable ()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+    private void Update()
+    {
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+        EngineAudio();
+    }
+
+    private void EngineAudio()
+    {
+        // Check if the tank is not moving
+        if(Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
+        {
+            // If idling is not playing, play it
+            if(m_MovementAudio.clip != m_EngineIdling)
+            {
+                PlayAudioClip(m_EngineIdling);
+            }
+        }
+        // Enter here if tank is moving
+        else
+        {
+            // If driving clip is not currently playing, play it
+            if(m_MovementAudio.clip != m_EngineDriving)
+            {
+                PlayAudioClip(m_EngineDriving);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0f, turn, 0f);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * rotation);
+    }
+
+    private void PlayAudioClip(AudioClip clip)
+    {
+        m_MovementAudio.clip = clip;
+        m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+        m_MovementAudio.Play();
+    }
+}
